d47896fa2a9c0012fe7aefdd0883a35011c5e08a
Luis Rodriguez <Luis.Rodriguez_Arguijo@telus.com>
2019-07-31 16:57:46 -0600

    Refactoring server project structure, organizing in folders

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 //Common-JS modules vs ES2015 modules
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
 
 const app = express();
-
-// To indicate passport to use Google Oauth.
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile", profile);
-    }
-  )
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+require("./routes/authRoutes")(app);
 
 // For dynamic port binding or 5000 by default.
 const PORT = process.env.PORT || 5000;
