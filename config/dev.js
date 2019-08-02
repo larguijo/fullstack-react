b8043c3ec23404023184cba87cfcc3c71f76e6d4
Luis <rodriguez.arguijo@gmail.com>
2019-08-02 15:51:21 -0600

    Separating environments to have DEV in local machine and Production in Heroku

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID:
+      "596085636579-151q37h1s7sjtjndj363j0l498e2holc.apps.googleusercontent.com",
+    googleClientSecret: "20P3sxMmg9ZgX-VLecXH14fp",
+    mongoURI:
+      "mongodb+srv://emaily-dev:emaily-dev@cluster0-sl3ue.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    cookieKey: "dkakggiiur787889898989"
+  };
+  
--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
-module.exports = {
-  googleClientID:
-    "596085636579-151q37h1s7sjtjndj363j0l498e2holc.apps.googleusercontent.com",
-  googleClientSecret: "20P3sxMmg9ZgX-VLecXH14fp",
-  mongoURI:
-    "mongodb+srv://emaily-dev:emaily-dev@cluster0-sl3ue.gcp.mongodb.net/test?retryWrites=true&w=majority",
-  cookieKey: "dkakggiiur787889898989"
-};
+// Figure out what set of credentials to return.
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod')
+} else {
+  module.exports = require('./dev');
+}
