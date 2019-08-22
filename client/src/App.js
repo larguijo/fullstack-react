0cd7af6180ad6c4db89f50cdd2eed8084d654d20
Luis Rodriguez <Luis.Rodriguez_Arguijo@telus.com>
2019-08-22 15:23:22 -0600

    React app added for application using create-react-app. Concurrently package added to run server and client at the same time. Proxy config was added to automaticaly process calls to the bff with relative path

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hi There</h2>
+        <a href="/auth/google">Sign In with Google</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
