b673fe7bff5e8d00599d13fb1e64c0a1703560e3
Luis Rodriguez <Luis.Rodriguez_Arguijo@telus.com>
2019-10-10 21:26:09 -0600

    Adding lastResponed field and handling Thanks for voting page

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,7 @@ const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = (app) => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting');
   });
 
@@ -63,7 +63,8 @@ module.exports = (app) => {
           },
           {
             $inc: { [choice]: 1 },
-            $set: { 'recipients.$.responded': true }
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
           }).exec();
       })
       .value();
